f8c4c7882be3c8ab1015cf79fbdfeaba6a117628
miloslazic1999 <miloslazic1999@gmail.com>
2021-01-09 21:09:53 +0100

    Admin panel dizain, evidencija i statistika

---
+++ DesktopApplication/Controls/InfoCard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApplication.Controls
+{
+	public partial class InfoCard : UserControl
+	{
+		public InfoCard()
+		{
+			InitializeComponent();
+			DataContext = this;
+		}
+		public string Value { get; set; }
+		public string Text { get; set; }
+	}
+}
