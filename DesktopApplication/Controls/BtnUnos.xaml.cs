a4dec26d12c19fde87280b00d95aa5a0c4911f47
Nikola Stevanovic <nidzaasa@gmail.com>
2021-01-10 21:25:19 +0100

    dodat buttonunos i infoCard

---
+++ DesktopApplication/Controls/BtnUnos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApplication.Controls
+{
+    /// <summary>
+    /// Interaction logic for BtnUnos.xaml
+    /// </summary>
+    public partial class BtnUnos : Page
+    {
+        public BtnUnos()
+        {
+            InitializeComponent();
+        }
+    }
+}
