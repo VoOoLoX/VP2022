cfddc63a091f4587071ee483037c930e1010429c
VoOoLoX <vooolox@gmail.com>
2021-01-13 19:58:27 +0100

    NewVehicle funkcionalnost

---
+++ BusinessLayer/ManufacturerBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shared;
+
+namespace BusinessLayer {
+	public static class ManufacturerBusiness {
+		public static List<Manufacturer> GetAll() => Repositories.Instance.ManufacturerRepository.GetAll();
+
+		public static Manufacturer Get(int id) => GetAll().FirstOrDefault(manufacturer => manufacturer.ID == id);
+
+		public static int Insert(Manufacturer manufacturer) => Repositories.Instance.ManufacturerRepository.InsertGetID(manufacturer);
+	}
+}
